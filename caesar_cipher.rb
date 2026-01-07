ee2dbcd342c4e06b972ef488ae254ba30ccc5d3f
Anna Gladush <ann.gladush@gmail.com>
2026-01-07 16:56:46 +0200

    Created a file with outline for a function

---
+++ caesar_cipher.rb
@@ -0,0 +1,7 @@
+def caesar_cipher(string, step)
+  # You will need to remember how to convert a string into a number.
+  # Don’t forget to wrap from z to a.
+  # Don’t forget to keep the same case.
+  # The Wikipedia quote discusses a Caesar cipher using a left shift.
+  
+end

138689197f79ae24d77fc141ebdf48183a887383
Anna Gladush <ann.gladush@gmail.com>
2026-01-12 18:38:10 +0200

    Finished Stock picker

--- 3_stock_picker.rb
+++ 3_stock_picker.rb
@@ -1,28 +1,38 @@
 def stock_picker(array)
-  # takes in an array of stock prices, one for each hypothetical day. It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
-  return "ERROR" if array.length < 2
+  return "ERROR" if array.length < 2 || (array.length == 2 && array[0] > array[1])
+  return [array.find_index(array.min), array.find_index(array.max)] if array.length == 2
 
-  
+  sell_day = 0
+  sell_idx = 0
+  buy_idx = 0
 
-  # sell_item = (array[1..-1].sort)[-1]
-  # p sell_idx = array.find_index(sell_item)
-  # p buy_item = array[0..sell_idx].sort[0]
-  # buy_idx = array.find_index(buy_item)
-  # [buy_idx, sell_idx]
+  # Sell index
 
+  array[1..-1].each_with_index do |day, i| 
+    if day >= sell_day
+      sell_day = day
+      sell_idx = i+1
+    end
+  end
 
-  # sell = array[1..-1].max
-  # sell_idx = array.find_index(sell)
-  # buy = array[0..sell_idx].min
-  # buy_idx = array.find_index(buy)
-  
-  # [buy_idx, sell_idx]
+  buy_day = sell_day
+  if buy_day < array[0]
+    buy_day = array[0]
+  end
 
+  # Buy index
+  array[0..sell_idx].each_with_index do |day, i|
+    if buy_day > day
+      buy_day = day
+      buy_idx = i
+    end
+  end
+  result = [buy_idx, sell_idx]
+  result
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10])
-p stock_picker([17])
-p stock_picker([9, 2, 2, 2, 1])
-p stock_picker([1, 2, 2, 2, 13])
-# => [1,4]  # for a profit of $15 - $3 == $12
-# 
+# p stock_picker([17,3,6,9,15,8,6,1,10])
+# p stock_picker([17])
+# p stock_picker([17, 5])
+# p stock_picker([9, 2, 2, 2, 1])
+# p stock_picker([1, 2, 2, 2, 13])

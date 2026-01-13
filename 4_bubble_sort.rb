ae3e2718bdce63e76e5b55950cc2912890168977
Anna Gladush <ann.gladush@gmail.com>
2026-01-13 11:47:49 +0200

    Fixed length

--- 4_bubble_sort.rb
+++ 4_bubble_sort.rb
@@ -2,7 +2,7 @@ def bubble_sort(array)
   save = 0
   return array if array.length <= 1
   
-  (array.length-1).times do
+  (array.length-2).times do
     (array.length-1).times do |index|
       if array[index] > array[index+1]
         save = array[index+1]
@@ -15,4 +15,4 @@ def bubble_sort(array)
 end
 
 p bubble_sort([4,3,78,2,0,2])
-# [0,2,2,3,4,78]
+p bubble_sort([4,3,78,2,0,2,100,1,3,5,3,6,0,9])

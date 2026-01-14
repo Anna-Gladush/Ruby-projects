3ea410de60a96ca876ed924f340e163806a756d0
Anna Gladush <ann.gladush@gmail.com>
2026-01-14 13:54:36 +0200

    Figured it won't work the way ot should if array is less than 4. Now fixed. Still not optimised

--- 4_bubble_sort.rb
+++ 4_bubble_sort.rb
@@ -2,7 +2,7 @@ def bubble_sort(array)
   save = 0
   return array if array.length <= 1
   
-  (array.length-2).times do
+  (array.length-1).times do
     (array.length-1).times do |index|
       if array[index] > array[index+1]
         save = array[index+1]

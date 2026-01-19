e358cd449a8186dd22871c281d849c7fb016be47
Anna Gladush <ann.gladush@gmail.com>
2026-01-19 17:51:04 +0200

    formatted bubble sort

--- 4_bubble_sort.rb
+++ 4_bubble_sort.rb
@@ -1,12 +1,12 @@
 def bubble_sort(array)
   save = 0
   return array if array.length <= 1
-  
-  (array.length-1).times do
-    (array.length-1).times do |index|
-      if array[index] > array[index+1]
-        save = array[index+1]
-        array[index+1] = array[index]
+
+  (array.length - 1).times do
+    (array.length - 1).times do |index|
+      if array[index] > array[index + 1]
+        save = array[index + 1]
+        array[index + 1] = array[index]
         array[index] = save
       end
     end

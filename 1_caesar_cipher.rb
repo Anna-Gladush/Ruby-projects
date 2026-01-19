6485a95c08f86f51ecf7e50d326a4714509f9edc
Anna Gladush <ann.gladush@gmail.com>
2026-01-19 17:28:30 +0200

    Fixed some of formating using rubocop

--- 1_caesar_cipher.rb
+++ 1_caesar_cipher.rb
@@ -1,32 +1,25 @@
 # By default step is chosen as 1.
-# caesar_cipher("Zorro!")
-# caesar_cipher("What a string!", 5)
-
-# caesar_decipher("Bmfy f xywnsl!", 5)
-# caesar_decipher("Apssp!")
-
 def caesar_cipher(string, step = 1)
-
   str_array = string.split.to_a
   new_words = []
   result = []
   str_array.each do |words|
     deciphered_letter = []
-    word = words.split("")
+    word = words.split('')
     word.each do |letter|
       if (letter.ord + step) > 90 && (65 <= letter.ord && letter.ord <= 90)
         letter_shifted = 65 + letter.ord + (step - 1) - 90
-        deciphered_letter.push((letter_shifted.ord).chr)
+        deciphered_letter.push(letter_shifted.ord.chr)
       elsif (letter.ord + step) > 122 && (97 <= letter.ord && letter.ord <= 122)
         letter_shifted = 97 + letter.ord + (step - 1) - 122
-        deciphered_letter.push((letter_shifted.ord).chr)
+        deciphered_letter.push(letter_shifted.ord.chr)
       elsif (65 <= letter.ord && letter.ord <= 90) || (97 <= letter.ord && letter.ord <= 122)
-        deciphered_letter.push((letter.ord + step).chr) 
+        deciphered_letter.push((letter.ord + step).chr)
       else
         deciphered_letter.push(letter)
       end
     end
-    new_words.push(deciphered_letter, " ")
+    new_words.push(deciphered_letter, ' ')
   end
   new_words.each do |words|
     if words.respond_to?(:to_a)
@@ -40,27 +33,26 @@ def caesar_cipher(string, step = 1)
 end
 
 def caesar_decipher(string, step = 1)
-
   str_array = string.split.to_a
   new_words = []
   result = []
   str_array.each do |words|
     deciphered_letter = []
-    word = words.split("")
+    word = words.split('')
     word.each do |letter|
       if (letter.ord - step) < 65 && (65 <= letter.ord && letter.ord <= 90)
-        letter_shifted =  90 - (65 + (step -1) - letter.ord)
-        deciphered_letter.push((letter_shifted.ord).chr)
+        letter_shifted = 90 - (65 + (step - 1) - letter.ord)
+        deciphered_letter.push(letter_shifted.ord.chr)
       elsif (letter.ord - step) < 97 && (97 <= letter.ord && letter.ord <= 122)
-        letter_shifted = 97 - (122 + (step -1) - letter.ord)
-        deciphered_letter.push((letter_shifted.ord).chr)
+        letter_shifted = 97 - (122 + (step - 1) - letter.ord)
+        deciphered_letter.push(letter_shifted.ord.chr)
       elsif (65 <= letter.ord && letter.ord <= 90) || (97 <= letter.ord && letter.ord <= 122)
-        deciphered_letter.push((letter.ord - step).chr) 
+        deciphered_letter.push((letter.ord - step).chr)
       else
         deciphered_letter.push(letter)
       end
     end
-    new_words.push(deciphered_letter, " ")
+    new_words.push(deciphered_letter, ' ')
   end
   new_words.each do |words|
     if words.respond_to?(:to_a)
@@ -70,5 +62,11 @@ def caesar_decipher(string, step = 1)
     end
   end
   result.pop
-  result.join    
+  result.join
 end
+
+# p caesar_cipher("Zorro!")
+# p caesar_cipher("What a string!", 5)
+
+# p caesar_decipher("Bmfy f xywnsl!", 5)
+# p caesar_decipher("Apssp!")

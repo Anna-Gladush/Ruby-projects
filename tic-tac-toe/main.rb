8f4ffd5a62fd9112eb2b19a0b65b980b45a8f14a
Anna Gladush <ann.gladush@gmail.com>
2026-01-20 22:00:34 +0200

    Figuring out some functions

--- tic-tac-toe/main.rb
+++ tic-tac-toe/main.rb
@@ -1,8 +1,14 @@
 # Table
-puts ' 1 | 2 | 3 '
-puts '-----------'
-puts ' 4 | 5 | 6 '
-puts '-----------'
-puts ' 7 | 8 | 9 '
-puts '-----------'
+a = ' 1 | 2 | 3 '
+b = '-----------'
+c = ' 4 | 5 | 6 '
+d = '-----------'
+e = ' 7 | 8 | 9 '
+f = '-----------'
+puts a, b, c, d, e, f
 # puts `clear` #will clear the output
+a =  a.gsub('1', 'X')
+c = c.gsub('6', 'O')
+c = c.gsub('4', 'O')
+puts c.count('O')
+puts a, b, c, d, e, f

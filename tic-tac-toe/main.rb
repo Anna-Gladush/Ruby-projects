41fcb832986df8c77c84a0dbec770a38885eda7f
Anna Gladush <ann.gladush@gmail.com>
2026-01-21 18:35:09 +0200

    basic sceleton for program

--- tic-tac-toe/main.rb
+++ tic-tac-toe/main.rb
@@ -1,54 +1,74 @@
 # Table
-a = ' 1 | 2 | 3 '
-b = '-----------'
-c = ' 4 | 5 | 6 '
-d = '-----------'
-e = ' 7 | 8 | 9 '
-f = '-----------'
-puts a, b, c, d, e, f
+a_row = ' 1 | 2 | 3 '
+b_row = '-----------'
+c_row = ' 4 | 5 | 6 '
+d_row = '-----------'
+e_row = ' 7 | 8 | 9 '
+f_row = '-----------'
+puts a_row, b_row, c_row, d_row, e_row, f_row
 
 used_numbers = []
-3.times do
-  puts 'Your turn: '
+x_positions = []
+o_positions = []
+
+i = 9
+while i >= 1
+  puts 'Player 1 turn: '
   answer = gets.chomp
-  if a.include?(answer)
-    a = a.gsub(answer, 'X')
-  elsif c.include?(answer)
-    c = c.gsub(answer, 'X')
-  elsif e.include?(answer)
-    e = e.gsub(answer, 'X')
+  if a_row.include?(answer)
+    a_row = a_row.gsub(answer, 'X')
+  elsif c_row.include?(answer)
+    c_row = c_row.gsub(answer, 'X')
+  elsif e_row.include?(answer)
+    e_row = e_row.gsub(answer, 'X')
   end
   used_numbers << answer
-  puts a, b, c, d, e, f
+  x_positions << answer
+  puts a_row, b_row, c_row, d_row, e_row, f_row
+  i -= 1
+
+  puts 'Player 2 turn: '
+  answer = gets.chomp
+  if a_row.include?(answer)
+    a_row = a_row.gsub(answer, 'O')
+  elsif c_row.include?(answer)
+    c_row = c_row.gsub(answer, 'O')
+  elsif e_row.include?(answer)
+    e_row = e_row.gsub(answer, 'O')
+  end
+  used_numbers << answer
+  o_positions << answer
+  puts a_row, b_row, c_row, d_row, e_row, f_row
+  i -= 1
 end
-p used_numbers
+
 # puts c.count('O')
 # puts `clear` #will clear the output
 
-class Player
-  def turn
-    answer = gets.chomp
-    while answer >= 1 && answer <= 9
-      used_numbers << answer
-    end
-  end
-  def symbol
-    sym = nil
-    until sym == 'X' || sym == 'O'
-      puts 'Pick "X" or "O"'
-      sym = gets.chomp
-      sym.upcase!
-    end
-    sym
-  end
-end
+# class Player
+#   def turn
+#     answer = gets.chomp
+#     while answer >= 1 && answer <= 9
+#       used_numbers << answer
+#     end
+#   end
+#   def symbol
+#     sym = nil
+#     until sym == 'X' || sym == 'O'
+#       puts 'Pick "X" or "O"'
+#       sym = gets.chomp
+#       sym.upcase!
+#     end
+#     sym
+#   end
+# end
 
-player1 = Player.new
-player2 = Player.new
-choice_of_p1 = player1.symbol
+# player1 = Player.new
+# player2 = Player.new
+# choice_of_p1 = player1.symbol
 
-if choice_of_p1 == 'X'
-  choice_of_p2 = 'O'
-else 
-  choice_of_p2 = 'X'
-end
+# if choice_of_p1 == 'X'
+#   choice_of_p2 = 'O'
+# else 
+#   choice_of_p2 = 'X'
+# end

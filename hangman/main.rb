fd2b1d85c0a7200f014e70a4c7813f79f54b52bb
Anna Gladush <ann.gladush@gmail.com>
2026-02-04 15:16:35 +0200

    Added function for user input

--- hangman/main.rb
+++ hangman/main.rb
@@ -15,3 +15,14 @@ guess_word = String.new('_' * word.length)
 puts guess_word
 
 try_left = 9
+
+def user_input
+  char = 'word'
+  # checks if input is a letter and its length is equal to 1.
+  ask = 'Please enter your guess (one letter): '
+  until char.length == 1 && char.match?(/[A-Za-z]/)
+    puts ask
+    char = gets.chomp
+  end
+end
+user_input

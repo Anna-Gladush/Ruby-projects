c928382b3d22b48257b25d535c2edf301576a749
Anna Gladush <ann.gladush@gmail.com>
2026-02-04 11:54:43 +0200

    Made a function random_word, added a condition (word length is between 5 and 12)

--- hangman/main.rb
+++ hangman/main.rb
@@ -1,9 +1,13 @@
-dictionary = []
-File.open('words.txt', 'r') do |file|
-  for line in file.readlines
-    dictionary << line
+def random_word
+  dictionary = []
+  File.open('words.txt', 'r') do |file|
+    file.readlines.each do |line|
+      dictionary << line
+    end
   end
+  dictionary.select! { |word| word.length >= 5 && word.length <= 12 }
+  dictionary.sample
 end
 
-word = dictionary.sample
+word = random_word
 puts word, word.length
